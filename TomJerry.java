d6a22616e55cef2d723d3ba7f6484a20c11a8011
Kruthinhk <129154935+Kruthinhk@users.noreply.github.com>
2023-04-03 12:07:39 +0530

    Add files via upload

---
+++ TomJerry.java
@@ -0,0 +1,17 @@
+class TomJerry
+ {
+  public static void main(String[] args)
+  {
+   int Tage=7;
+   double Theight=4.5;
+   char TGender='m';
+
+   int Jage=6;
+   double Jheight=2;
+   char JGender='f';
+
+   System.out.print("TOM \n age= " + Tage + " ,height= " + Theight + " Gender = " + TGender + "\n");
+   System.out.print("Jerry \n age= " + Jage + " ,height= " + Jheight + " Gender = " + JGender);
+  }
+ } 
+
